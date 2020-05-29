47fba64e7544d04a4640153c0f5e621811ef42c8
luguangdongw <lgd15095370993@gmail.com>
2020-05-29 18:22:27 +0800

    数据核心业务测试

---
+++ beyond-repository-core/src/test/java/com/beyond/repository/core/CoreAdminMapperTests.java
@@ -0,0 +1,15 @@
+package com.beyond.repository.core;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author luguangdong
+ * @version 1.0.0
+ * @ClassName CoreAdminMapperTests
+ * @date 2020/5/29 18:21
+ * @company https://www.beyond.com/
+ */
+public class CoreAdminMapperTests {
+}
